83ee7dffd9cd6ae589cf2a74a7f80c85f13cbe8d
Lippiece <seriouss7222@gmail.com>
2022-06-12 13:16:37 +0200

    init with boilers

---
+++ scripts.js
@@ -0,0 +1,22 @@
+/*
+Arrays
+*/
+const board       = [[],[],[]],
+	// sections        = document.querySelectorAll(".section"),
+	// controlsSection = sections[0],
+	// boardSection = sections[1];
+
+/*
+"-er" functions: walker, talker...
+*/
+// function cross(){}
+// function circle(){}
+/* Redraw not the whole board every time
+but those points with bool check true */
+// function drawBoard(){}
+/*
+Subject functions to use "-er" functions
+*/
+// function player1(){}
+// function player2(){}
+// function
