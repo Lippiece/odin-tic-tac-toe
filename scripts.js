84374619acf67e21665790b31179b5988bcdf02e
Lippiece <seriouss7222@gmail.com>
2022-06-18 15:31:03 +0200

    style: provide initial structure to the scripts
    with commentary sections

--- scripts.js
+++ scripts.js
@@ -1,22 +1,35 @@
 /*
-Arrays
+* Arrays
 */
 const board       = [[],[],[]],
-	// sections        = document.querySelectorAll(".section"),
-	// controlsSection = sections[0],
-	// boardSection = sections[1];
+/*
+* Check variables
+*/
+/*
+* Control event listeners
+*/
+/*
+* Initializer
+*/
+/*
+* Main block
+*/
+{
+	/*
+	* Support functions
+	*/
+	/*
+	* -er functions
 
-/*
-"-er" functions: walker, talker...
-*/
-// function cross(){}
-// function circle(){}
-/* Redraw not the whole board every time
-but those points with bool check true */
-// function drawBoard(){}
-/*
-Subject functions to use "-er" functions
-*/
-// function player1(){}
-// function player2(){}
-// function
+ 	*/
+	/*
+	* Fills the board with empty content
+	*/
+		/*
+		* Adds event listener to a field
+		*/
+		/*
+
+		* Field object constructor
+		*/
+}
